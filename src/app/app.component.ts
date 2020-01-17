86e27e1fa4b848a8d1782b8e4cc4ec23735b26e8
Ashleigh Christopher <ashleighc207@gmail.com>
2020-01-17 11:41:34 -0500

    make cards into components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: "Cool Trees",
+      user: "TreeMan",
+      descr: "This was a really cool shot I got while hiking. Enjoy!",
+      image: "assets/trees.jpg"
+    },
+    {
+      title: "Mountains Meeting",
+      user: "TheRealBigFoot",
+      descr: "What a view!! I mean just look at that teal glassy water!",
+      image: "assets/mountains.jpg"
+    },
+    {
+      title: "Stars for Days",
+      user: "CampingForACause",
+      descr:
+        "This was a gorgeous night sky while camping on a glacier in Alaska.",
+      image: "assets/night.jpeg"
+    }
+  ];
 }
